ce0798169ce5f73c2a3520ca3ffe8d9d15138992
aaediie <raj229488@gmail.com>
2022-01-01 16:55:17 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
 
 
 function App() {
@@ -38,21 +38,21 @@ function App() {
   return (
     <>
     {/* <Navbar title ="textutils2" aboutText="Aboutus" />   */}
-    <Router> 
+    {/* <Router>  */}
     <Navbar title = "TEXTUTILS" mode={mode} toggleMode={toggleMode}/>
     <Alert alert= {alert}/>
     <div className="container my-7">
-    <Switch>
+    {/* <Switch>
           <Route exact path="/about">
             <About />
           </Route>
-          <Route exact path="/">
+          <Route exact path="/"> */}
            <Textform heading="Enter your text to analyze" mode = {mode} showAlert={showAlert}/>
-          </Route>
+          {/* </Route>
         </Switch>
-        
+         */}
     </div>
-    </Router>
+    {/* </Router> */}
     </>
   );
 }
